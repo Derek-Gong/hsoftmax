7a24f0e5af44f86ba491b92144f3fd3092f90e86
derekgong.dg@gmail.com <derekgong.dg@gmail.com>
2022-02-18 15:22:03 +0900

    Critical correction from path_mask_sign to path_mask_bias

--- wenet/transformer/hsoftmax_layer.py
+++ wenet/transformer/hsoftmax_layer.py
@@ -7,6 +7,7 @@ from wenet.utils.multiprocessing import ProcessPool, Worker
 from queue import PriorityQueue
 import time
 
+
 class HSoftmaxLayer(nn.Module):
     def __init__(
         self,
@@ -14,32 +15,35 @@ class HSoftmaxLayer(nn.Module):
         attention_dim: int,
         tree: HuffmanTree,
         num_workers: int,
-    ): 
+    ):
         assert tree.info()['leaf_cnt'] == vocab_size
         super().__init__()
 
         self.tree = tree
         self.inner_vector = nn.Linear(attention_dim, tree.inner_cnt, False)
-        self.register_buffer('path_mask_sign', torch.Tensor(tree.path_mask_sign))
-        self.register_buffer('path_mask_bias', torch.Tensor(tree.path_mask_bias))
-        
+        self.register_buffer(
+            'path_mask_sign', torch.Tensor(tree.path_mask_sign))
+        self.register_buffer(
+            'path_mask_bias', torch.Tensor(tree.path_mask_bias))
+
         self.num_workers = mp.cpu_count() if num_workers is None else num_workers
         self.pool = None
         self.queue = None
 
-    def forward(self, att: torch.Tensor): 
+    def forward(self, att: torch.Tensor):
         h = self.inner_vector(att)
         h = torch.sigmoid(h)
         # h.size = [inner_cnt], path_mask.size = [vocab_size, inner_cnt]
         H = h * self.path_mask_sign
         # H.size = [vocab_size, inner_cnt]
-        H = H + self.path_mask_sign
+        H = H + self.path_mask_bias
         return torch.sum(torch.log(H), -1)
 
     def beam_search(self, att: torch.Tensor, beam_size: int) -> List[List[int]]:
         if self.pool is None:
             self.inner_vector = self.inner_vector.to('cpu')
-            self.pool = ProcessPool(self.num_workers, SearchWorker, self.inner_vector, self.tree.root, beam_size)
+            self.pool = ProcessPool(
+                self.num_workers, SearchWorker, self.inner_vector, self.tree.root, beam_size)
         att = att.to('cpu')
         logps, indexs = [], []
         # start = time.time()
@@ -54,21 +58,24 @@ class HSoftmaxLayer(nn.Module):
             logps.append(top_k_logp)
             indexs.append(top_k_index)
         # print('beam', time.time()-start)
-        return torch.FloatTensor(logps), torch.LongTensor(indexs) # (B*N, N)
+        return torch.FloatTensor(logps), torch.LongTensor(indexs)  # (B*N, N)
+
 
 class SearchWorker(Worker):
     def __init__(self, in_queue, out_queue, inner_vector, root, beam_size):
         super().__init__(in_queue, out_queue)
-        self.inner_vector = inner_vector # pickle these could be time consuming?
+        self.inner_vector = inner_vector  # pickle these could be time consuming?
         self.root = root
         self.beam_size = beam_size
 
-    def __call__(self, att, idx = None):
-        assert att.dim() == 1 # batch size should be 1, att should an attention for only 1 token
+    def __call__(self, att, idx=None):
+        assert att.dim() == 1  # batch size should be 1, att should an attention for only 1 token
+
         class Candidate:
             def __init__(self, prob, node):
                 self.prob = prob
                 self.node = node
+
             def __lt__(self, other):
                 return self.prob < other.prob
         candidates = PriorityQueue()
@@ -77,27 +84,25 @@ class SearchWorker(Worker):
 
         while not candidates.empty():
             cur = candidates.get()
-            
-            if cur.node.token is not None: # leaf node
+
+            if cur.node.token is not None:  # leaf node
                 tokens.put(cur)
                 while tokens.qsize() > self.beam_size:
                     tokens.get()
-            else: # inner node
-                prob_left = torch.sigmoid(torch.dot(att, self.inner_vector.weight[cur.node.idx])).item()
+            else:  # inner node
+                prob_left = torch.sigmoid(
+                    torch.dot(att, self.inner_vector.weight[cur.node.idx])).item()
                 prob_right = 1.0 - prob_left
                 candidates.put(Candidate(prob_left, cur.node.left))
                 candidates.put(Candidate(prob_right, cur.node.right))
 
                 while candidates.qsize() > self.beam_size:
                     candidates.get()
-            
+
         top_k_logp, top_k_index = [], []
         while not tokens.empty():
             cur = tokens.get()
             top_k_logp.append(cur.prob)
             top_k_index.append(cur.node.idx)
-        
+
         return top_k_logp, top_k_index
-
-
-
